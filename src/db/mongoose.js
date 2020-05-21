4c439376b0ae74ed33c24f8ab985fc9ad9fbe62d
Aaditya <aadityarajv@gmail.com>
2020-05-21 17:55:38 +0530

    Refreshing pacakages

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_DB_URL, {
     useUnifiedTopology: true,
     useCreateIndex: true,
+    useNewUrlParser: true,
     useFindAndModify: false
 });
 
