dbb5a3b813f631dff3fe8e1b728ae0a035fab4ee
Aaditya <aadityarajv@gmail.com>
2020-05-22 22:36:16 +0530

    Fixed mongoose error

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
 
+const options = {
+   useNewUrlParser: true,
+   useCreateIndex: true,
+   autoIndex: true,
+   keepAlive: true,
+   connectTimeoutMS: 10000,
+   socketTimeoutMS: 45000,
+   useFindAndModify: false,
+   useUnifiedTopology: true 
+}
 
-mongoose.connect(process.env.MONGO_DB_URL, {
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useFindAndModify: false
-});
-
-
+mongoose.connect(process.env.MONGO_DB_URL, options);
 
 
 // const me = new User({
